f780bf96e1f074f93bf013c76969d5493a8e74d6
Rafael <rafael.c@lyncas.net>
2023-05-03 18:01:09 -0300

    Correção da estruração e erros

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+import 'animate.css'
+import '@/assets/css/tailwind.css'
+import '@/assets/css/fonts.css'
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
