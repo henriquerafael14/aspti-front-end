c64bb5a3ce52b5ea5438c7cd100106c912603cdd
Rafael <ra.costa@catolicasc.edu.br>
2023-05-14 13:17:49 -0300

    Inicio de tela de login

---
+++ src/views/Home/Login.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="flex items-center justify-center w-full h-full bg-black bg-opacity-50">
+        <div class="mx-10">
+            <div class="flex flex-col overflow-hidden bg-white rounded-lg animate__animated animate__fadeIn animate__faster">
+                <div class="flex flex-col px-12 py-10 bg-white">
+                    <div class="mt-16">
+                        <form @submit.prevent="handleSubmit">
+                            <label class="block">
+                                <span class="text-lg font-medium text-gray-800">
+                                    <input v-model="state.email.value" type="email" :class="{'border-brand-danger': !!state.email.errorMessage}" class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded" placeholder="E-mail">
+                                </span>
+                                <span v-if="!!state.email.errorMessage" class="block font-medium text-brand-danger">
+                                    {{ state.email.errorMessage }}
+                                </span>
+                            </label>
+                            <label class="block mt-9">
+                                <span class="text-lg font-medium text-gray-800">
+                                    <input v-model="state.password.value" type="email" :class="{'border-brand-danger': !!state.password.errorMessage}" class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded" placeholder="Senha">
+                                </span>
+                                <span v-if="!!state.password.errorMessage" class="block font-medium text-brand-danger">
+                                    {{ state.password.errorMessage }}
+                                </span>
+                            </label>
+
+                            <button :disabled="state.isLoading" :class="{ 'opacity-50': state.isLoading }" class="px-8 py-3 mt-10 text-2xl font-bold text-white rounded-full bg-brand-main focus:outline-none ">Entrar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { reactive } from 'vue'
+import { useField } from 'vee-validate'
+import { validateEmptyAndLength3, validateEmptyAndEmail } from '../../utils/validators'
+export default {
+  setup () {
+    const {
+      value: emailValue,
+      errorMessage: emailErrorMessage
+    } = useField('email', validateEmptyAndEmail)
+    const {
+      value: passwordValue,
+      errorMessage: passwordErrorMessage
+    } = useField('password', validateEmptyAndLength3)
+    const state = reactive({
+      hasErrors: false,
+      isLoading: false,
+      email: {
+        value: emailValue,
+        errorMessage: emailErrorMessage
+      },
+      password: {
+        value: passwordValue,
+        errorMessage: passwordErrorMessage
+      }
+    })
+    function handleSubmit () {
+    }
+    return {
+      state,
+      handleSubmit
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,22 +1,14 @@
 <template>
-    <CustomHearder
-        @create-account="handleAccountCreate"
-        @login="handleLogin"
-    />
-    <Contact />
-    <div class="flex justify-center py-10 bg-brand-gray">
-        <p class="front-medium text-center text-gray-800">Aspti | 2023</p>
-    </div>
+  <Login />
 </template>
 
 <script>
 import { onMounted } from 'vue'
-import Contact from './Contact.vue'
-import CustomHearder from './CustomHearder.vue'
+import Login from './Login.vue'
 import { useRouter } from 'vue-router'
 
 export default {
-  components: { Contact, CustomHearder },
+  components: { Login },
   setup () {
     const router = useRouter()
 
